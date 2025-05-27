63de2fc2288dd250a62d23f1cabacc03fa5c0baa
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-05-27 22:52:22 +0300

    Add вбудовані модулі

--- index.js
+++ index.js
@@ -1,12 +1,64 @@
-// console.log(5+5)
+const os = require("os");
+const fs = require("fs");
 
-// const sum = require('./math')
-// console.log(sum(8, 3));
+// console.log(os.cpus());
 
-// const Math = require("./math");
-// console.log(Math.sum(20, 20));
-// console.log(Math.mult(3, 3));
+// const fileText = fs.readFileSync("./math.js", { encoding: "utf-8" });
+// console.log(fileText);
 
-const { sum, mult } = require("./math");
-console.log(sum(3, 20));
-console.log(mult(8, 8));
+// const fileRead = fs.readFile(
+//   "./match.js",
+//   { encoding: "utf-8" },
+//   (error, data) => {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log(data);
+//     }
+//   }
+// );
+// console.log(fileRead);
+
+const read = fs.readdir(".", (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    data
+      .filter((f) => /^.*\.js$/.test(f))
+      .forEach((f) =>
+        fs.readFile(f, { encoding: "utf-8" }, (error, data) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log(data);
+          }
+        })
+      );
+  }
+});
+
+console.log(read);
+
+//Переписати в синхронний код
+try {
+  const files = fs.readdirSync(".");
+  // console.log('Files in directory:');
+  files
+    .filter((f) => /^.*\.js$/.test(f))
+    .forEach((f) => {
+      try {
+        const data = fs.readFileSync(f, { encoding: "utf-8" });
+        // console.log('Data from file:', f);
+        // console.log(data);
+      } catch (err) {
+        // console.log('Error reading file:', f, err);
+      }
+    });
+} catch (err) {
+  console.log("Error reading directory:", err);
+}
+
+console.log(__filename);
+console.log(__dirname);
+
+console.log(process);
