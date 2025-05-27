0d27ffbabdc138812ad3162db4373103f52a9101
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-05-27 21:30:45 +0300

    Add export/import

---
+++ index.js
@@ -0,0 +1,12 @@
+// console.log(5+5)
+
+// const sum = require('./math')
+// console.log(sum(8, 3));
+
+// const Math = require("./math");
+// console.log(Math.sum(20, 20));
+// console.log(Math.mult(3, 3));
+
+const { sum, mult } = require("./math");
+console.log(sum(3, 20));
+console.log(mult(8, 8));
---
+++ math.js
@@ -0,0 +1,22 @@
+// const sum = (a, b) => {
+//   return a + b;
+// };
+
+// const mult = (a, b) => {
+//   return a * b;
+// };
+
+// module.exports = sum;
+
+// module.exports = { sum,mult };
+
+//export const sum = (a, b) => {
+//     return a + b;
+//   };
+
+//export const mult = (a, b) => {
+//     return a * b;
+//   };
+
+module.exports.sum = (a, b) => a + b;
+module.exports.mult = (a, b) => a * b;
