23a5a6ce0b7875d4c5c6a78ab712d5a033040b1c
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-05-28 23:15:31 +0300

    Add request httpServer

--- index.js
+++ index.js
@@ -1,64 +1,14 @@
-const os = require("os");
-const fs = require("fs");
+const http = require("http");
 
-// console.log(os.cpus());
+const httpServer = http.createServer((request,response) => {
+  console.log(request.url)
+  console.log(request.method)
 
-// const fileText = fs.readFileSync("./math.js", { encoding: "utf-8" });
-// console.log(fileText);
-
-// const fileRead = fs.readFile(
-//   "./match.js",
-//   { encoding: "utf-8" },
-//   (error, data) => {
-//     if (error) {
-//       console.log(error);
-//     } else {
-//       console.log(data);
-//     }
-//   }
-// );
-// console.log(fileRead);
-
-const read = fs.readdir(".", (error, data) => {
-  if (error) {
-    console.log(error);
-  } else {
-    data
-      .filter((f) => /^.*\.js$/.test(f))
-      .forEach((f) =>
-        fs.readFile(f, { encoding: "utf-8" }, (error, data) => {
-          if (error) {
-            console.log(error);
-          } else {
-            console.log(data);
-          }
-        })
-      );
-  }
+  response.statusCode =200
+  response.setHeader('ContentType','playn/text')
+  response.end('result')
 });
 
-console.log(read);
-
-//Переписати в синхронний код
-try {
-  const files = fs.readdirSync(".");
-  // console.log('Files in directory:');
-  files
-    .filter((f) => /^.*\.js$/.test(f))
-    .forEach((f) => {
-      try {
-        const data = fs.readFileSync(f, { encoding: "utf-8" });
-        // console.log('Data from file:', f);
-        // console.log(data);
-      } catch (err) {
-        // console.log('Error reading file:', f, err);
-      }
-    });
-} catch (err) {
-  console.log("Error reading directory:", err);
-}
-
-console.log(__filename);
-console.log(__dirname);
-
-console.log(process);
+httpServer.listen(5000, "localhost", () => {
+  console.log('Server is listening port 5000,host "localhost"');
+});
