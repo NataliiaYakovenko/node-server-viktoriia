1fc89b1f6ae37957ccac8b3854e54d5c86260ea3
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-05-29 20:49:47 +0300

    Add requestListener

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
-const { readFile } = require("fs/promises");
 const http = require("http");
+const requestListener = require("./requestListener");
 
-const httpServer = http.createServer((request, response) => {
-  console.log(request.url);
-  console.log(request.method);
-
-  readFile("./index.html", { encoding: "utf-8" })
-    .then((data) => {
-      response.statusCode = 200;
-      response.setHeader("ContentType", "playn/text");
-      response.end(data);
-    })
-    .catch((error) => {
-      response.statusCode = 500;
-      response.setHeader("ContentType", "playn/text");
-      response.end("file had not read");
-    });
-
-  // response.end('result')
-});
+const httpServer = http.createServer(requestListener);
 
 httpServer.listen(5000, "localhost", () => {
   console.log('Server is listening port 5000,host "localhost"');
