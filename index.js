ee50331c422e90df99ca72632a6f2624d0d3ca1d
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-05-29 20:19:22 +0300

    Add read file html

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+const { readFile } = require("fs/promises");
 const http = require("http");
 
-const httpServer = http.createServer((request,response) => {
-  console.log(request.url)
-  console.log(request.method)
+const httpServer = http.createServer((request, response) => {
+  console.log(request.url);
+  console.log(request.method);
 
-  response.statusCode =200
-  response.setHeader('ContentType','playn/text')
-  response.end('result')
+  readFile("./index.html", { encoding: "utf-8" })
+    .then((data) => {
+      response.statusCode = 200;
+      response.setHeader("ContentType", "playn/text");
+      response.end(data);
+    })
+    .catch((error) => {
+      response.statusCode = 500;
+      response.setHeader("ContentType", "playn/text");
+      response.end("file had not read");
+    });
+
+  // response.end('result')
 });
 
 httpServer.listen(5000, "localhost", () => {
